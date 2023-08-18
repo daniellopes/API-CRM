dad0be861909b5e3bc053038c26a12520bb57372
daniellopes <dl.lopes2011@gmail.com>
2023-08-18 12:33:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,3 @@
-
-import bodyParser from "body-parser";
-import cors from "cors";
-import dotenv from "dotenv";
-import helmet from "helmet";
-import morgan from "morgan";
-import userRoutes from "./routes/accounts.js";
-import clientsRoutes from "./routes/clients.js";
-import serviceRoutes from "./routes/services.js";
-import contactRoutes from "./routes/contacts.js";
-import contractRoutes from "./routes/contracts.js";
-import contactsByQuantity from "./routes/contactsByQuantity.js";
-
-// data imports
-import User from "./models/User.js";
-import Clients from "./models/Clients.js";
-import Contacts from "./models/Contact.js";
-import Contracts from "./models/Contract.js";
-import ProductStat from "./models/ProductStat.js";
-import OverallStat from "./models/OverallStat.js";
-import AffiliateStat from "./models/AffiliateStat.js";
-import Services from "./models/Service.js";
-import ContactByQuantity from "./models/ContactByQuantity.js";
-import {
-  dataUser,
-  //dataProduct,
-  //dataProductStat,
-  //dataTransaction,
-  //dataOverallStat,
-  //dataAffiliateStat,
-  dataServices,
-  dataClient,
-  dataContacts,
-  dataContracts,
-} from "./data/index.js";
-import { getUser, getUsers } from "./controllers/users.js";
-
-/* CONFIGURATION */
-dotenv.config();
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
-
-/* ROUTES */
-app.use("/accounts", userRoutes);
-app.use("/clients", clientsRoutes);
-app.use("/services", serviceRoutes);
-app.use("/contacts", contactRoutes);
-app.use("/contracts", contractRoutes);
-app.use("/contactsByQuantity", contactsByQuantity);
-
-/* MONGOOSE SETUP */
 const express = require('express')
 const mongoose = require('mongoose')
 
@@ -61,7 +6,7 @@ const PORT = process.env.PORT || 3000
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
